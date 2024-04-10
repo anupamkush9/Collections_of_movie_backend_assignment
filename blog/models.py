b31d3f1736fce4466c366766d69af734c53a7ce1
Anupamkush9 <anupam.kush9@gmail.com>
2024-04-10 11:32:50 +0530

    custom permission in django model meta implemented

--- blog/models.py
+++ blog/models.py
@@ -3,4 +3,11 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=400)
     body = models.TextField()
-
+    is_published = models.BooleanField(default=False)
+    class Meta:
+        permissions = [
+            (
+                "set_published_status",
+                "Can set the status of the post to either publish or not"
+            )
+        ]
