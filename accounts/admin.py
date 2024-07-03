d24a8afe07682ddadaac7f32bcf0f71f62a145c3
Anupamkush9 <anupam.kush9@gmail.com>
2024-07-03 17:29:46 +0530

    done with creating dynamic permission based on a title of post model

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from django.contrib.auth.models import Permission, User
 from django.db import transaction
 from .models import UserPermissionCredit
+from django.contrib.auth.models import Permission
+from django.contrib.contenttypes.models import ContentType
+from django.apps import apps
 
 class UserPermissionCreditAdmin(admin.ModelAdmin):
     fields = ['user', 'permission', 'credits']
@@ -16,18 +19,44 @@ class UserPermissionCreditAdmin(admin.ModelAdmin):
                 # Retrieve permission and user details
                 permission_name = obj.permission
                 user = obj.user
+        
+                # Get all models in the project
+                all_models = apps.get_models()
+                all_models_name = [model_name.__name__.lower() for model_name in all_models]
+                # Print the names of all models
+                for model in all_models:
+                    print(model.__name__)
+                        
+                if permission_name in all_models_name:
+                    # Assign the add and view permissions to the user
+                    for each_permission in ["add", "view"]:
+                        model_permission_code_name = f"{each_permission}_{permission_name}"
+                        model_permission = Permission.objects.get(codename__iexact=model_permission_code_name)
+                        user.user_permissions.add(model_permission)
+                        # Ref : https://testdriven.io/blog/django-permissions/ 
+                        # Ref : https://testdriven.io/blog/django-permissions/ 
+                else:  
+                    # changes for post title wise permission
+                    from blog.models import Post
+                    post_titles = Post.objects.all().values_list('title', flat=True)
+                    post_titles = [(post_title,post_title.replace("_"," ")) for post_title in post_titles]
 
-                # Assign the add and view permissions to the user
-                for each_permission in ["add", "view"]:
-                    model_permission_code_name = f"{each_permission}_{permission_name}"
-                    model_permission = Permission.objects.get(codename__iexact=model_permission_code_name)
+                    # Create or extend permissions with these titles
+                    content_type = ContentType.objects.get_for_model(Post)
+
+                    for original_title, modified_title in post_titles:
+                        permission, created = Permission.objects.get_or_create(
+                            codename=f'can_view_{original_title.lower()}',
+                            name=f'Can view {modified_title}',
+                            content_type=content_type,
+                        )
+                    code_name = f"can_view_{permission_name}"
+                    model_permission = Permission.objects.get(codename__iexact=code_name)
                     user.user_permissions.add(model_permission)
-                    # Ref : https://testdriven.io/blog/django-permissions/ 
-                    # Ref : https://testdriven.io/blog/django-permissions/ 
         except Permission.DoesNotExist:
-            print(f"Permission {model_permission_code_name} does not exist.")
+            print(f"Permission does not exist.")
         except Exception as e:
-            print("Exception is", e)
+            print("Exception in UserPermissionCreditAdmin", e)
             import traceback
             print(traceback.format_exc())
 
@@ -36,14 +65,25 @@ class UserPermissionCreditAdmin(admin.ModelAdmin):
             with transaction.atomic():
                 user = obj.user
                 permission_name = obj.permission
-                # Remove the permissions associated with the UserPermissionCredit instance
-                for each_permission in ["add", "view"]:
-                    model_permission_code_name = f"{each_permission}_{permission_name}"
-                    model_permission = Permission.objects.get(codename__iexact=model_permission_code_name)
+                # Get all models in the project
+                all_models = apps.get_models()
+                all_models_name = [model_name.__name__.lower() for model_name in all_models]
+                # Print the names of all models
+                for model in all_models:
+                    print(model.__name__)
+                        
+                if permission_name in all_models_name:
+                    # Remove the permissions associated with the UserPermissionCredit instance
+                    for each_permission in ["add", "view"]:
+                        model_permission_code_name = f"{each_permission}_{permission_name}"
+                        model_permission = Permission.objects.get(codename__iexact=model_permission_code_name)
+                        user.user_permissions.remove(model_permission)
+                        # Ref : https://testdriven.io/blog/django-permissions/ 
+                        # Ref : https://testdriven.io/blog/django-permissions/ 
+                else:
+                    code_name = f"can_view_{permission_name}"
+                    model_permission = Permission.objects.get(codename__iexact=code_name)
                     user.user_permissions.remove(model_permission)
-                    # Ref : https://testdriven.io/blog/django-permissions/ 
-                    # Ref : https://testdriven.io/blog/django-permissions/ 
-                    
                 # Now delete the UserPermissionCredit object
                 obj.delete()
         except Permission.DoesNotExist:
